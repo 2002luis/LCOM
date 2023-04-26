1f483e6d63931e0d7e0e4f3946c1ce82e0e8a178
zeta <ti.gouveia21@gmail.com>
2023-04-26 12:29:51 +0100

    lab5 struggle

---
+++ lab5/video_gr.c
@@ -0,0 +1,125 @@
+#include <lcom/lcf.h>
+
+#include "video_gr.h"
+#include "keyboard.h"
+
+static void *video_mem;
+static int vram_size;
+static int bytes_per_pixel;
+vbe_mode_info_t vmi;
+
+extern int kbd_read;
+
+void *(vg_init)(uint16_t mode) {
+  reg86_t reg86;
+
+  memset(&reg86, 0, sizeof(reg86));
+
+  reg86.ax = 0x4f02;
+  reg86.bx = (1 << 14) | mode;
+  reg86.intno = 0x10;
+
+  if(sys_int86(&reg86) != OK) {
+    printf("\tvg_init(): sys_int86() failed \n");
+    return NULL;
+  }
+  return NULL;
+}
+
+int (map_vram)(uint16_t mode) {
+  struct minix_mem_range mr;
+
+  unsigned int vram_base;  /* VRAM's physical addresss */
+
+  //unsigned int vram_size;
+  /* VRAM's size, but you can use
+              the frame-buffer size, instead */
+  int r;
+
+  /* Use VBE function 0x01 to initialize vram_base and vram_size */
+
+  if(vbe_get_mode_info(mode,&vmi)!=OK) {
+    return 1;
+  }
+
+  vram_base = vmi.PhysBasePtr;
+  bytes_per_pixel = vmi.BytesPerScanLine / vmi.XResolution;
+  vram_size = (vmi.XResolution * vmi.YResolution) * bytes_per_pixel;
+
+  /* Allow memory mapping */
+
+  mr.mr_base = (phys_bytes) vram_base;
+  mr.mr_limit = mr.mr_base + vram_size;  
+
+  if( OK != (r = sys_privctl(SELF, SYS_PRIV_ADD_MEM, &mr))){
+    panic("sys_privctl (ADD_MEM) failed: %d\n", r);
+        return 1;
+  }
+
+  /* Map memory */
+
+  video_mem = vm_map_phys(SELF, (void *)mr.mr_base, vram_size);
+
+  if(video_mem == MAP_FAILED) {
+    panic("couldn't map video memory");
+    return 1;
+  }
+
+  return 0;
+}
+
+int(vg_draw_hline)(uint16_t x, uint16_t y, uint16_t len, uint32_t color) {
+  uint8_t bytes = (vmi.BitsPerPixel + 7) >> 3;
+  uint8_t* base = (uint8_t*) video_mem +(y * vmi. XResolution + x) * bytes;
+
+  for(uint16_t i = 0; 1 < len; ++i) {
+    for(uint8_t j = 0; j < bytes; ++j) {
+      *base = color >> (j * 8);
+      ++base;
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
+
+int (vg_draw_rectangle)(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color) {
+  if (y + height > vmi.YResolution)
+    height = vmi.YResolution - y;
+  
+  if (x + width > vmi.XResolution)
+    width = vmi.XResolution - x;
+
+  for(uint16_t i = y; i < y + height + i; i++) {
+    vg_draw_hline(x, i, width, color);
+  }
+
+  return EXIT_SUCCESS;
+}
+
+int(kbd_loop)() {
+  int ipcstatus, driver;
+  uint8_t coco = 2;
+  uint32_t hookid = BIT(coco);
+  message msg;
+
+  kbd_subscribe(&coco);
+
+  while(kbd_read != 0x81) {
+    if((driver = driver_receive(ANY,&msg,&ipcstatus))!=0) {
+      printf("Erro a ler");
+    } else { //else
+      if(is_ipc_notify(ipcstatus)) {
+        switch(_ENDPOINT_P(msg.m_source)) {
+          case HARDWARE: 
+            if(msg.m_notify.interrupts & hookid) {
+              kbc_ih();
+            }
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+  return kbd_unsubscribe();
+}
